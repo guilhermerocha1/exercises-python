49fe321cedb83f926bf592668085776d5be60e88
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2023-01-16 16:32:45 -0300

    exemplos de course

---
+++ redes-raspagem-de-dados/dia-2-raspagem/src/description.py
@@ -0,0 +1,17 @@
+from parsel import Selector
+import requests
+
+URL_BASE = 'https://books.toscrape.com/catalogue/'
+
+response = requests.get(URL_BASE + 'page-1.html')
+selector = Selector(text=response.text)
+
+href = selector.css(".product_pod h3 a::attr(href)").get()
+detail_page_url = URL_BASE + href
+
+details_response = requests.get(detail_page_url)
+details_selector = Selector(text=details_response.text)
+
+details = details_selector.css(".product_page p::text").get()
+
+print(details)
